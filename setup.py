f9527dbc2638e31c62cc9fb03dd0efafd0a15732
Jonathan Hartley <tartley@tartley.com>
2010-04-29 01:15:49 +0100

    add setup.py classifiers for python2 and python3

--- setup.py
+++ setup.py
@@ -34,7 +34,9 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.1',
         'Topic :: Terminals',
     ]
