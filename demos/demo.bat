b95a3df31c8b2d0b3e2bb8b302bd46ccca477a79
Dang <daniel.dang.griffith@gmail.com>
2011-04-16 22:33:31 -0400

    Moved demos into their own directory, extracted them from the bash file, and added a Window batch version of the bash file.
    --HG--
    rename : demo.sh => demos/demo.sh

---
+++ demos/demo.bat
@@ -0,0 +1,21 @@
+@rem!/usr/bin/env bash
+
+@rem Script to demonstrate features of colorama.
+
+@rem This demo is also used to verify correctness visually, because we don't have automated tests.
+
+@rem Implemented as a bash script which invokes python so that we can test the
+@rem behaviour on exit, which resets default colors again.
+
+python demo01.py
+
+python demo02.py
+
+python demo03.py
+
+python demo04.py
+
+python demo05.py
+
+python demo06.py
+
---
+++ demos/demo.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Script to demonstrate features of colorama.
+
+# This demo is also used to verify correctness visually, because we don't have automated tests.
+
+# Implemented as a bash script which invokes python so that we can test the
+# behaviour on exit, which resets default colors again.
+
+python demo01.py
+
+python demo02.py
+
+python demo03.py
+
+python demo04.py
+
+python demo05.py
+
+python demo06.py
+
