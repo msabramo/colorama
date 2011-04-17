982a41d18629bb3035be00c9a5ba3a0e0b1d2cf9
Dang <daniel.dang.griffith@gmail.com>
2011-04-16 23:10:52 -0400

    Fixed batch file to redirect stderr for demo04.py.

--- demos/demo.bat
+++ demos/demo.bat
@@ -13,9 +13,10 @@ python demo02.py
 
 python demo03.py
 
-python demo04.py
+if exist demo04.out del demo04.out
+python demo04.py 2> demo04.out
+type demo04.out
 
 python demo05.py
 
 python demo06.py
-
